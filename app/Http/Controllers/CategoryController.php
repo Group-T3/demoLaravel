abe5e71aa45072d4a663a36f6c53a0d28d31397e
unknown <ngodaix5tp@gmail.com>
2022-12-29 10:42:55 +0700

    Second commit.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,31 +10,37 @@ class CategoryController extends Controller
 {
     private CategoryServiceInterfaces $categoryServiceInterfaces;
 
-    public function __construct(CategoryServiceInterfaces $categoryServiceInterfaces) {
+    public function __construct(CategoryServiceInterfaces $categoryServiceInterfaces)
+    {
         $this->categoryServiceInterfaces = $categoryServiceInterfaces;
     }
 
-    public function getList() {
+    public function getList()
+    {
         $category = $this->categoryServiceInterfaces->findAll();
         return view('templates.product.category.list')->with('categories', $category);
     }
 
-    public function getDetail($id) {
+    public function getDetail($id)
+    {
         $category = $this->categoryServiceInterfaces->findById($id);
         return view('templates.product.category.detail')->with('category', $category);
     }
 
-    public function createCategory(CategoryRequest $request) {
+    public function createCategory(CategoryRequest $request)
+    {
         $validated = $request->validated();
         return $this->categoryServiceInterfaces->create($validated);
     }
 
-    public function updateCategory($id, CategoryRequest $request) {
+    public function updateCategory($id, CategoryRequest $request)
+    {
         $validated = $request->validated();
         return $this->categoryServiceInterfaces->update($id, $validated);
     }
 
-    public function deleteCategory($id) {
+    public function deleteCategory($id)
+    {
         $this->categoryServiceInterfaces->delete($id);
     }
 }
